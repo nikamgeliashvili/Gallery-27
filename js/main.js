d15e4093b57a60d1697af35a96c2591259a31833
nikamgeliashvili <mgeliashvilia@gmail.com>
2019-11-13 11:48:21 +0400

    fixed navigation

--- js/main.js
+++ js/main.js
@@ -83,3 +83,18 @@ span.onclick = function() {
 }
 
 
+
+window.onscroll = function() {myFunction()};
+    
+var navbar = document.getElementById("nav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+if (window.pageYOffset >= sticky) {
+navbar.classList.add("sticky")
+} else {
+navbar.classList.remove("sticky");
+}
+}
+
+
